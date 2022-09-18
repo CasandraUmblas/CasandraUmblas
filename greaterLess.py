aee26eeb60456160d1b52b332ee162123cb6c865
CasandraUmblas <113647710+CasandraUmblas@users.noreply.github.com>
2022-09-18 18:37:43 +0800

    Create greaterLess.py

---
+++ greaterLess.py
@@ -0,0 +1,9 @@
+num_1 = int (input("Enter the first number: "))
+num_2 = int (input("Enter the second number: "))
+
+if (num_1 > num_2):
+    print (num_1, "is greater than", num_2)
+elif (num_1 < num_2):
+    print (num_1, "is less than", num_2)
+elif (num_1 == num_2):
+    print (num_1, "is equal to", num_2)
